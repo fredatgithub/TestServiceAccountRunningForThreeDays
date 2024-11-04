74aec348d29c8a29f6715daa2e37b061041b87e9
Freddy JUHEL <ext.freddy.juhel@reseau.sncf.fr>
2024-11-04 12:18:56 +0100

    change labels

--- TestServiceAccountRunningForThreeDays/Program.cs
+++ TestServiceAccountRunningForThreeDays/Program.cs
@@ -17,7 +17,7 @@ namespace TestServiceAccountRunningForThreeDays
       Display("Voici un test d'application console pour tester si une tâche lancée par un compte de service peut durer 3 jours");
       var startDate = DateTime.Now;
       var endDate = startDate.AddDays(3);
-      logger.Info("Début de l'application");
+      logger.Info("Début du traitement");
       while (DateTime.Now < endDate)
       {
         string message = $"log = {DateTime.Now}";
@@ -25,7 +25,7 @@ namespace TestServiceAccountRunningForThreeDays
         Thread.Sleep(60000);
       }
 
-      logger.Info("fin de l'application");
+      logger.Info("fin du traitement");
     }
   }
 }
